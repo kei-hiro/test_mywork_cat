25c86a760d586da2a9e9883cd2da710d000b90ac
kh <94734274+kei-hiro@users.noreply.github.com>
2022-02-28 17:40:22 +0900

    Add files via upload

---
+++ js/page-top.js
@@ -0,0 +1,20 @@
+  jQuery(function() {
+    var pagetop = $('#page-top');   
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+
+    /* 今回は機能させない
+    pagetop.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500); //0.5秒かけてトップへ移動
+        return false;
+    });
+    */
+});
---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(function () {
+    $(".slider").slick({
+      autoplay: true,
+      dots: true,
+      fade: true,
+      speed: 2000,
+      autoplaySpeed: 2500,
+      arrows: false,
+      infinite: true,
+      pauseOnHover: false,
+    });
+  });
